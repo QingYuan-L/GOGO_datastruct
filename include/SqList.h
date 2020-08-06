cd3f4b83cd68e5fc813c63456ec248f7bd520f48
qingyuan <QingYuan-L@users.noreply.github.com>
2020-08-06 18:32:33 +0800

    编写动态链接库测试

---
+++ include/SqList.h
@@ -0,0 +1,17 @@
+//
+// Created by admin on 2020/8/6.
+//
+
+#ifndef GOGO_DATASTRUCT_SQLIST_H
+#define GOGO_DATASTRUCT_SQLIST_H
+
+#define ElemType int
+#define MaxSize 50
+typedef struct
+{
+    ElemType data[MaxSize];
+    int length;
+};
+
+void CreatSqList(SqList )
+#endif //GOGO_DATASTRUCT_SQLIST_H
